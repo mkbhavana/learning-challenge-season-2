3958271320025306fb9f8db9d30b741d1a99c641
Bhavana <mkbhavana1999@gmail.com>
2019-07-03 12:04:17 -0700

    added gitignore and excercise files

---
+++ excercise.py
@@ -0,0 +1,226 @@
+name = "john"
+-..-..- - /.-.-.-.-.--
+if name == "john" and age == 23:
+    print("your name is john, and you are 23 years old.")
+    if name == "john" or name == "Rick":
+        print("your name is john or rick.")
+
+-..-..- - /.-.-.-.-.--
+x = [1, 2, 3]
+-..-..- - /.-.-.-.-.--
+y = [1, 2, 3]
+-..-..- - /.-.-.-.-.--
+print(x == y)
+-..-..- - /.-.-.-.-.--
+print(x is y)
+-..-..- - /.-.-.-.-.--
+primes = [2, 3, 5, 7]
+-..-..- - /.-.-.-.-.--
+for prime in primes:
+    print(prime)
+
+-..-..- - /.-.-.-.-.--
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+-..-..- - /.-.-.-.-.--
+for x in range(5):
+    print(x)
+
+-..-..- - /.-.-.-.-.--
+for x in range(3, 6):
+    print(x)
+
+-..-..- - /.-.-.-.-.--
+for x in range(3, 5, 7):
+    print(x)
+
+-..-..- - /.-.-.-.-.--
+for x in range(3, 8, 2):
+    print(x)
+
+-..-..- - /.-.-.-.-.--
+while count < 5:
+    print(count)
+    count += 1
+
+-..-..- - /.-.-.-.-.--
+while True:
+    print(couint)
+    count += 1
+    if count >= 5:
+        break
+
+-..-..- - /.-.-.-.-.--
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+
+-..-..- - /.-.-.-.-.--
+while (count < 5):
+    print(count)
+    count += 1
+else:
+    print("count value reached %d"( % count))
+
+    -..-..- - /.-.-.-.-.--
+    count = 0
+    -..-..- - /.-.-.-.-.--
+    while (count < 5):
+        print(count)
+        count += 1
+    else:
+        print("count value reached %d" % (count))
+
+    -..-..- - /.-.-.-.-.--
+    mylist = [orange]
+    -..-..- - /.-.-.-.-.--
+    mylist = []
+    -..-..- - /.-.-.-.-.--
+    mylist.append(1)
+    -..-..- - /.-.-.-.-.--
+    mylist.append(2)
+    -..-..- - /.-.-.-.-.--
+    mylist.append(3)
+    -..-..- - /.-.-.-.-.--
+    print(mylist[0])
+    -..-..- - /.-.-.-.-.--
+    print(mylist[2])
+    -..-..- - /.-.-.-.-.--
+    x = 1, 2, 3
+    -..-..- - /.-.-.-.-.--
+    for x in range mylist:
+        print(x)
+
+    -..-..- - /.-.-.-.-.--
+    print(x)
+
+-..-..- - /.-.-.-.-.--
+for x in mylist:
+    print(x)
+
+-..-..- - /.-.-.-.-.--
+print(mylist[10])
+-..-..- - /.-.-.-.-.--
+print(mylist[0 1])
+
+wrt
+-..-..- - /.-.-.-.-.--
+print(mylist[1])
+-..-..- - /.-.-.-.-.--
+number = 1 + 2 * 3 / 0.4
+-..-..- - /.-.-.-.-.--
+print(number)
+-..-..- - /.-.-.-.-.--
+remainder = 11 % 3
+-..-..- - /.-.-.-.-.--
+print(remainder)
+-..-..- - /.-.-.-.-.--
+cubed = 2 ** * 3
+print(squared)
+-..-..- - /.-.-.-.-.--
+squared = 7 ** 2
+-..-..- - /.-.-.-.-.--
+cubed = 2 ** 3
+-..-..- - /.-.-.-.-.--
+print(squared)
+-..-..- - /.-.-.-.-.--
+print(cubed)
+-..-..- - /.-.-.-.-.--
+string = "hello" + " " + "world"
+-..-..- - /.-.-.-.-.--
+print(string)
+-..-..- - /.-.-.-.-.--
+lotsofhellos = "hello" * 10
+-..-..- - /.-.-.-.-.--
+print(lotsofhellos)
+-..-..- - /.-.-.-.-.--
+lotsofhellos = "hello" * " " * 10
+-..-..- - /.-.-.-.-.--
+lotsofhellos = "hello" * " "
+-..-..- - /.-.-.-.-.--
+all_numbers = even_numbers + odd_numbers
+-..-..- - /.-.-.-.-.--
+even_numbers = [2, 4, 6, 8]
+-..-..- - /.-.-.-.-.--
+odd_number = [1, 3, 5, 7]
+-..-..- - /.-.-.-.-.--
+all_numbers = even_numbers + odd_number
+-..-..- - /.-.-.-.-.--
+print(all_numbers)
+-..-..- - /.-.-.-.-.--
+prinr([1, 2, 3] * 3)
+-..-..- - /.-.-.-.-.--
+print([1, 2, 3] * 3)
+-..-..- - /.-.-.-.-.--
+print("hello, %s" % name)
+-..-..- - /.-.-.-.-.--
+name = "John"
+-..-..- - /.-.-.-.-.--
+age = 23
+-..-..- - /.-.-.-.-.--
+print("%s is %d years old." % (name, age))
+-..-..- - /.-.-.-.-.--
+mylist = [1, 2, 3]
+-..-..- - /.-.-.-.-.--
+print("A list: %s" % mylist)
+-..-..- - /.-.-.-.-.--
+print("string quotes are ' '")
+-..-..- - /.-.-.-.-.--
+print(len(astring))
+-..-..- - /.-.-.-.-.--
+astring = "Hello World!"
+print(astring.indes("0"))
+-..-..- - /.-.-.-.-.--
+astring = "Hello World!"
+print(astring.index("0"))
+-..-..- - /.-.-.-.-.--
+print(astring.index("0"))
+-..-..- - /.-.-.-.-.--
+print(astring.index("o"))
+-..-..- - /.-.-.-.-.--
+print(astring.count("1"))
+-..-..- - /.-.-.-.-.--
+print(astring[3:7:2])
+-..-..- - /.-.-.-.-.--
+print(astring[3:7])
+-..-..- - /.-.-.-.-.--
+print(astring[3:7:1])
+-..-..- - /.-.-.-.-.--
+print(astring.upper())
+-..-..- - /.-.-.-.-.--
+print(astring.lower())
+-..-..- - /.-.-.-.-.--
+print(astring.startswith("Hello"))
+-..-..- - /.-.-.-.-.--
+print(astring.startswith("asdfasdfasdf"))
+-..-..- - /.-.-.-.-.--
+astring = "Hello World!"
+-..-..- - /.-.-.-.-.--
+afewwords = astring.split(" ")
+-..-..- - /.-.-.-.-.--
+
+
+def my_function():
+    print("Hello from My Function!")
+
+
+-..-..- - /.-.-.-.-.--
+
+
+def my_function_with_args(username, greeting):
+    print("Hello,%s,From my function!, I wish you %s" % (username, greeting))
+
+
+-..-..- - /.-.-.-.-.--
+
+
+def sum_two_numbers(a, b:)
+    return a + b
+
+
+-..-..- - /.-.-.-.-.--
+
+
+def sum_two_numbers(a, b):
+    return a + b
