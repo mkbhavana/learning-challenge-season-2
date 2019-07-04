c8fcd51c93ed39a53973f82bbabcccfbbb6021a2
Bhavana <mkbhavana1999@gmail.com>
2019-07-04 02:34:24 -0700

    updated todays topic

---
+++ day4.py
@@ -0,0 +1,54 @@
+class Myclass:
+    variabke = "blah"
+    def function(self):
+        print("This is a message inside the class.")
+class Myclass:
+variabl = "blah"
+def function(self)
+print("This is a message inside the class.")
+myobject = Myclass()
+class Myclass:
+variable = "blah"
+def function(self):
+print("this message is from console")
+myobjectx = Myclass()
+myobject.variable
+
+
+class Myclass:
+variable = "blah"
+print("This message is from console")
+myobject = Myclass()
+print(myobject.variable)
+
+class Myclass:
+variable = "blah"
+print("This message is from console")
+myobjectx = Myclass()
+myobjecty = Myclass()
+myobjecty.variable = "yackity"
+print(myobjectx.variable)
+print(myobjecty.variable)
+
+class Myclass:
+variable = "blah"
+def function(self):
+print("This message is from console")
+myobjectx = Myclass()
+myobjectx.function()
+
+phonebook = {}
+phonebook["John"] = 938477566
+phonebook["jack"] = 938377264
+print(phonebook)
+
+phonebook= {"John": 938477566, "Jack": 938377264}
+print(phonebook)
+
+phonebook{"John" = 938477566, "Jack" = 938377264}
+for name,number in phonebook.items():
+print("phonenumber of %s is %d" %(name,number))
+
+phonebook = {"John" = 938477566, "Jack" = 938377264}
+del phonebook["John"]
+print(phonebook)
