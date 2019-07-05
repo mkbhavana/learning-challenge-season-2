ed8d0f0799cce6b37f26fda5d7af551ed30e3bfe
Bhavana <mkbhavana1999@gmail.com>
2019-07-05 03:41:42 -0700

    updated my readme and day5 files

--- README.md
+++ README.md
@@ -17,3 +17,8 @@ version control
 ## Day 4
 classes and objects - accessing object variables, accessing object functions.
 Dictionaries - iterating over dictionaries, removing a value.
+
+## Day 5
+Modules - writing modules, importing module objects to the current namespace, importing all objects from a module, custom import name, module initialization, extending module load path, exploring built-in modules, help function.
+Writing packages
+Reverse of a string
---
+++ day5.py
@@ -0,0 +1,44 @@
+def reverse(s):
+    return s[::-1]
+def ispalindrome (s):
+    rev = reverse(s)
+if (s == rev):
+    return true
+s = "malayalam"
+ans ispalindrome (s)
+if ans == 1:
+    print("yes")
+else:
+    print("no")
+
+
+#module with one function and one dictionary
+def greeting(name):
+    printr("Hello", +name)
+    person = {"name" : "John", "age" : 36, "country" : "Norway"}
+
+
+#package
+ class Mammals:
+     def __int__(self):
+         public()
+         self.member = ['Tiger', 'Elephant', 'Lion']
+      def printmembers(self):
+          print('printing members of the mammals class')
+          for member in self.members:
+              print('\t%s' %member)
+
+class Birds:
+    def __int__(self):
+        public()
+        self.members = ['sparrow', 'robin', 'duck']
+        def printmembers(self):
+            print('printing members of birds class')
+            for members in self.members:
+                print('\t%s' %members)
+from Animals import Mammals
+from Animals import Birds
+myMammal = Mammals()
+myMammal.printMembers()
+myBirds = Birds()
+myBird,printMembers()
